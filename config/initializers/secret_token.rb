123b6847975c9d300e9059359d3b5cb9c2e76a7e
rm2fpt <your.rm2fpt@gmail.com>
2014-04-30 19:50:08 +0100

    Iniciar repositório

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '77c1c964c7ff23a7656dd45674f78ef7926d465d85d2b996eaa30725ef648384efa28813ce6e042a8e353ea0d5975dd9b364d488583e7e2206228d5e62451304'
